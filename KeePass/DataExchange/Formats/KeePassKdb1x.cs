bf0facf1db07d199f8314fb672a1909856dce7a8
Roger Ye <shugaoye@qq.com>
2020-01-23 14:20:41 +0800

    KeePass 2.04-Alpha

---
+++ KeePass/DataExchange/Formats/KeePassKdb1x.cs
@@ -0,0 +1,82 @@
+/*
+  KeePass Password Safe - The Open-Source Password Manager
+  Copyright (C) 2003-2008 Dominik Reichl <dominik.reichl@t-online.de>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+using KeePass.Resources;
+
+using KeePassLib;
+using KeePassLib.Interfaces;
+using KeePassLib.Utility;
+
+namespace KeePass.DataExchange.Formats
+{
+	public sealed class KeePassKdb1x : FormatImporter
+	{
+		public override string FormatName { get { return "KeePass KDB (1.x)"; } }
+		public override string DefaultExtension { get { return "kdb"; } }
+		public override string AppGroup { get { return PwDefs.ShortProductName; } }
+
+		public override bool SupportsUuids { get { return true; } }
+		public override bool RequiresKey { get { return true; } }
+
+		public override Image SmallIcon
+		{
+			get { return KeePass.Properties.Resources.B16x16_KeePass; }
+		}
+
+		public override bool TryBeginImport()
+		{
+			Exception exLib;
+			if(!Kdb3File.IsLibraryInstalled(out exLib))
+			{
+				MessageService.ShowWarning(KPRes.KeePassLibCNotFound,
+					KPRes.KDB3KeePassLibC, exLib);
+
+				return false;
+			}
+
+			return true;
+		}
+
+		public override void Import(PwDatabase pwStorage, Stream sInput,
+			IStatusLogger slLogger)
+		{
+			string strTempFile = Path.GetTempFileName();
+			try { File.Delete(strTempFile); }
+			catch(Exception) { }
+
+			BinaryReader br = new BinaryReader(sInput);
+			byte[] pb = br.ReadBytes((int)sInput.Length);
+			br.Close();
+			File.WriteAllBytes(strTempFile, pb);
+
+			Kdb3File kdb3 = new Kdb3File(pwStorage, slLogger);
+			kdb3.Load(strTempFile);
+
+			try { File.Delete(strTempFile); }
+			catch(Exception) { }
+		}
+	}
+}
---
+++ KeePass/UI/UIUtil.cs
@@ -0,0 +1,228 @@
+/*
+  KeePass Password Safe - The Open-Source Password Manager
+  Copyright (C) 2003-2008 Dominik Reichl <dominik.reichl@t-online.de>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+using KeePass.Native;
+using KeePass.Util;
+
+using KeePassLib;
+
+namespace KeePass.UI
+{
+	public static class UIUtil
+	{
+		public static void RtfSetSelectionLink(RichTextBox richTextBox)
+		{
+			try
+			{
+				NativeMethods.CHARFORMAT2 cf = new NativeMethods.CHARFORMAT2();
+				cf.cbSize = (UInt32)Marshal.SizeOf(cf);
+
+				cf.dwMask = NativeMethods.CFM_LINK;
+				cf.dwEffects = NativeMethods.CFE_LINK;
+
+				IntPtr wParam = (IntPtr)NativeMethods.SCF_SELECTION;
+				IntPtr lParam = Marshal.AllocCoTaskMem(Marshal.SizeOf(cf));
+				Marshal.StructureToPtr(cf, lParam, false);
+
+				NativeMethods.SendMessage(richTextBox.Handle,
+					NativeMethods.EM_SETCHARFORMAT, wParam, lParam);
+
+				Marshal.FreeCoTaskMem(lParam);
+			}
+			catch(Exception) { Debug.Assert(false); }
+		}
+
+		public static Image CreateColorBitmap24(int nWidth, int nHeight, Color color)
+		{
+			Bitmap bmp = new Bitmap(nWidth, nHeight, PixelFormat.Format24bppRgb);
+
+			using(SolidBrush sb = new SolidBrush(color))
+			{
+				using(Graphics g = Graphics.FromImage(bmp))
+				{
+					g.FillRectangle(sb, 0, 0, nWidth, nHeight);
+				}
+			}
+
+			return bmp;
+		}
+
+		public static ImageList BuildImageList(List<PwCustomIcon> vImages,
+			int nWidth, int nHeight)
+		{
+			ImageList imgList = new ImageList();
+
+			imgList.ImageSize = new Size(nWidth, nHeight);
+			imgList.ColorDepth = ColorDepth.Depth32Bit;
+
+			foreach(PwCustomIcon pwci in vImages)
+			{
+				Image imgNew = pwci.Image;
+
+				if(imgNew == null) { Debug.Assert(false); continue; }
+
+				if((imgNew.Width != nWidth) || (imgNew.Height != nHeight))
+					imgNew = new Bitmap(imgNew, new Size(nWidth, nHeight));
+
+				imgList.Images.Add(imgNew);
+			}
+
+			return imgList;
+		}
+
+		public static ImageList ConvertImageList24(ImageList vSourceImages,
+			int nWidth, int nHeight, Color clrBack)
+		{
+			ImageList vNew = new ImageList();
+			vNew.ImageSize = new Size(nWidth, nHeight);
+			vNew.ColorDepth = ColorDepth.Depth24Bit;
+
+			SolidBrush brushBk = new SolidBrush(clrBack);
+
+			foreach(Image img in vSourceImages.Images)
+			{
+				Bitmap bmpNew = new Bitmap(nWidth, nHeight, PixelFormat.Format24bppRgb);
+
+				using(Graphics g = Graphics.FromImage(bmpNew))
+				{
+					g.FillRectangle(brushBk, 0, 0, nWidth, nHeight);
+
+					if((img.Width == nWidth) && (img.Height == nHeight))
+						g.DrawImageUnscaled(img, 0, 0);
+					else
+					{
+						g.InterpolationMode = InterpolationMode.High;
+						g.DrawImage(img, 0, 0, nWidth, nHeight);
+					}
+				}
+
+				vNew.Images.Add(bmpNew);
+			}
+
+			return vNew;
+		}
+
+		public static ImageList CloneImageList(ImageList ilSource, bool bCloneImages)
+		{
+			Debug.Assert(ilSource != null); if(ilSource == null) throw new ArgumentNullException("ilSource");
+
+			ImageList ilNew = new ImageList();
+			ilNew.ColorDepth = ilSource.ColorDepth;
+			ilNew.ImageSize = ilSource.ImageSize;
+
+			foreach(Image img in ilSource.Images)
+			{
+				if(bCloneImages) ilNew.Images.Add((Image)img.Clone());
+				else ilNew.Images.Add(img);
+			}
+
+			return ilNew;
+		}
+
+		public static bool DrawAnimatedRects(Rectangle rectFrom, Rectangle rectTo)
+		{
+			bool bResult;
+
+			try
+			{
+				NativeMethods.RECT rnFrom = new NativeMethods.RECT(rectFrom);
+				NativeMethods.RECT rnTo = new NativeMethods.RECT(rectTo);
+
+				bResult = NativeMethods.DrawAnimatedRects(IntPtr.Zero,
+					NativeMethods.IDANI_CAPTION, ref rnFrom, ref rnTo);
+			}
+			catch(Exception) { Debug.Assert(false); bResult = false; }
+
+			return bResult;
+		}
+
+		private static void SetCueBanner(IntPtr hWnd, string strText)
+		{
+			Debug.Assert(strText != null); if(strText == null) throw new ArgumentNullException();
+
+			try
+			{
+				IntPtr pText = Marshal.StringToHGlobalUni(strText);
+				NativeMethods.SendMessage(hWnd, NativeMethods.EM_SETCUEBANNER,
+					IntPtr.Zero, pText);
+				Marshal.FreeHGlobal(pText); pText = IntPtr.Zero;
+			}
+			catch(Exception) { Debug.Assert(false); }
+		}
+
+		public static void SetCueBanner(TextBox tb, string strText)
+		{
+			SetCueBanner(tb.Handle, strText);
+		}
+
+		public static void SetCueBanner(ToolStripTextBox tb, string strText)
+		{
+			SetCueBanner(tb.TextBox, strText);
+		}
+
+		public static void SetCueBanner(ToolStripComboBox tb, string strText)
+		{
+			try
+			{
+				NativeMethods.COMBOBOXINFO cbi = new NativeMethods.COMBOBOXINFO();
+				cbi.cbSize = Marshal.SizeOf(cbi);
+
+				NativeMethods.GetComboBoxInfo(tb.ComboBox.Handle, ref cbi);
+
+				SetCueBanner(cbi.hwndEdit, strText);
+			}
+			catch(Exception) { Debug.Assert(false); }
+		}
+
+		public static Bitmap CreateScreenshot()
+		{
+			Screen s = Screen.PrimaryScreen;
+			Bitmap bmp = new Bitmap(s.Bounds.Width, s.Bounds.Height);
+
+			using(Graphics g = Graphics.FromImage(bmp))
+			{
+				g.CopyFromScreen(s.Bounds.Location, new Point(0, 0),
+					s.Bounds.Size);
+			}
+
+			return bmp;
+		}
+
+		public static void DimBitmap(Bitmap bmp)
+		{
+			using(Brush b = new SolidBrush(Color.FromArgb(192, Color.Black)))
+			{
+				using(Graphics g = Graphics.FromImage(bmp))
+				{
+					g.FillRectangle(b, 0, 0, bmp.Width, bmp.Height);
+				}
+			}
+		}
+	}
+}
