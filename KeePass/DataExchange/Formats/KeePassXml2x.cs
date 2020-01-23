c4a7adf4edc12e5530d27a3eedd1c7933afd512b
Roger Ye <shugaoye@qq.com>
2020-01-23 14:19:36 +0800

    KeePass 2.05-Alpha

---
+++ KeePass/DataExchange/Formats/KeePassXml2x.cs
@@ -0,0 +1,54 @@
+/*
+  KeePass Password Safe - The Open-Source Password Manager
+  Copyright (C) 2003-2008 Dominik Reichl <dominik.reichl@t-online.de>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+using KeePass.Resources;
+
+using KeePassLib;
+using KeePassLib.Interfaces;
+using KeePassLib.Serialization;
+
+namespace KeePass.DataExchange.Formats
+{
+	internal sealed class KeePassXml2x : FormatImporter
+	{
+		public override string FormatName { get { return "KeePass XML (2.x)"; } }
+		public override string DefaultExtension { get { return "xml"; } }
+		public override string AppGroup { get { return PwDefs.ShortProductName; } }
+
+		public override bool SupportsUuids { get { return true; } }
+
+		public override Image SmallIcon
+		{
+			get { return KeePass.Properties.Resources.B16x16_Binary; }
+		}
+
+		public override void Import(PwDatabase pwStorage, Stream sInput,
+			IStatusLogger slLogger)
+		{
+			Kdb4File kdb4 = new Kdb4File(pwStorage);
+			kdb4.Load(sInput, Kdb4Format.PlainXml, slLogger);
+		}
+	}
+}
